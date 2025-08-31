71a958707038e00677183b8276458c1b8b75d4cd
fan92rus <fan92rus@mail.ru>
2025-08-31 20:47:40 +0700

    publish to repo

--- LibHandler/Models/Book.cs
+++ LibHandler/Models/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibHandler.Models
+﻿namespace LibHandler.Models
 {
     #pragma warning disable IDE1006
     #pragma warning disable CS8618
